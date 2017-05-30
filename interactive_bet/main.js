1adb18e629f429e08a15e36fbbe47f386bfb51fb
KulikovAlexej <aleksey.kulikov.96@gmail.com>
2017-05-30 17:22:48 +0300

    it works

--- interactive_bet/main.js
+++ interactive_bet/main.js
@@ -1,56 +1,122 @@
 APP = angular.module('App', ['ngRoute']);
 
-APP.controller('Panel', function($scope){
+APP.controller('Panel', function($scope, $http){
 	$scope.webSite = {
 		first: 'BetExplorer',
 		second: 'Sports'
 	}
-	$scope.selectedMatch = null;
+	$scope.init = function(){
+		$http({
+			method: 'GET',
+			url: 'http://localhost:5000/API/betExp.json'
+		}).then(function(res){
+			$scope.betExp = res.data.matches;
+			return $http({
+				method: 'GET',
+				url: 'http://localhost:5000/API/sports.json'
+			})
+		}).then(function(res){
+			$scope.sportsArr = res.data.matches;
+			$scope.sortArray($scope.betExp);
+			$scope.sortArray($scope.sportsArr);
+			console.log($scope.betExp);
+			$scope.sources = {
+				first : $scope.betExp,
+				second : $scope.sportsArr
+			};
+		});
+		
+	}
+	
+	$scope.selectedMatch = {
+		first: null,
+		second: null
+	};
+	$scope.firstMatch = null;
+	$scope.secondMatch = null;
+	$scope.oursMatches = [];
 	$scope.match_day = new Date();
 	$scope.leagues = ['РФПЛ', 'Английская Премьер-Лига', 'Серия А', 'Испанская Примера', 'Бундеслига', 'Лига чемпионов', 'Лига Европы'];
-	$scope.betExp = [
-		{league: 'Бундеслига', owner: 'Shal\'ke-04', guest: 'Nurnberg', score: '1:1', date: '23.05.2017', time: '18:00'},
-		{league: 'РФПЛ', owner: 'Zenit', guest: 'CSKA', score: '2:2', date: '24.05.2017', time: '18:00'},
-		{league: 'РФПЛ', owner: 'Зенит', guest: 'Краснодар', score: '1:0', date: '23.05.2017', time: '18:00'},
-		{league: 'РФПЛ', owner: 'Zenit', guest: 'CSKA', score: '2:2', date: '23.05.2017', time: '18:00'},
-		{league: 'Бундеслига', owner: 'Shal\'ke-04', guest: 'Nurnberg', score: '1:1', date: '23.05.2017', time: '18:00'},
-		{league: 'РФПЛ', owner: 'Zenit', guest: 'CSKA', score: '2:2', date: '24.05.2017', time: '18:00'},
-		{league: 'Бундеслига', owner: 'Shal\'ke-04', guest: 'Nurnberg', score: '1:1', date: '23.05.2017', time: '18:00'},
-		{league: 'Премьер-Лига', owner: 'Манчестер Юнайтед', guest: 'Манчестер Сити', score: '2:2', date: '23.05.2017', time: '18:00'},
-		{league: 'Серия А', owner: 'Милан', guest: 'Ювентус', score: '1:1', date: '25.05.2017', time: '18:00'},
-		{league: 'РФПЛ', owner: 'Спартак', guest: 'Локомотив', score: '2:2', date: '23.05.2017', time: '18:00'}
-	];
-	$scope.sportsArr = [
-		{league: 'Бундеслига', owner: 'Shal\'ke-04', guest: 'Nurnberg', score: '1:1', date: '23.05.2017', time: '18:00'},
-		{league: 'РФПЛ', owner: 'Zenit', guest: 'CSKA', score: '2:2', date: '23.05.2017', time: '18:00'},
-		{league: 'РФПЛ', owner: 'Зенит', guest: 'Краснодар', score: '1:0', date: '23.05.2017', time: '18:00'},
-		{league: 'РФПЛ', owner: 'Zenit', guest: 'CSKA', score: '2:2', date: '23.05.2017', time: '18:00'},
-		{league: 'Бундеслига', owner: 'Shal\'ke-04', guest: 'Nurnberg', score: '1:1', date: '23.05.2017', time: '18:00'},
-		{league: 'РФПЛ', owner: 'Zenit', guest: 'CSKA', score: '2:2', date: '23.05.2017', time: '18:00'},
-		{league: 'Бундеслига', owner: 'Shal\'ke-04', guest: 'Nurnberg', score: '1:1', date: '23.05.2017', time: '18:00'},
-		{league: 'Премьер-Лига', owner: 'Манчестер Юнайтед', guest: 'Манчестер Сити', score: '2:2', date: '23.05.2017', time: '18:00'},
-		{league: 'Серия А', owner: 'Милан', guest: 'Ювентус', score: '1:1', date: '23.05.2017', time: '18:00'},
-		{league: 'РФПЛ', owner: 'Спартак', guest: 'Локомотив', score: '2:2', date: '23.05.2017', time: '18:00'}
-	];
-	$scope.sources = {
-		first : $scope.betExp,
-		second : $scope.sportsArr
-	};
+	$scope.sortArray = function(array){
+		array.sort(function(a,b){
+			if(a.league > b.league){
+				return 1
+			}
+			if(a.league < b.league){
+				return -1
+			}
+			else{
+				if(a.owner > b.owner){
+					return 1
+				}
+				if(a.owner > b.owner){
+					return -1
+				}
+				else{return 0}
+			}
+	})
+	}
+	
+	
+	
 	$scope.selectMatch = function(match){
 		$scope.sources.first.forEach(function(match){
-			match.class = 'disabled';
+			match.active = false;
 		});
 		if(match != null) {
 			
 		}
-		$scope.selectedMatch = match;
-		match.class = 'active';
-		console.log(match.league);
+		$scope.selectedMatch.first = match;
+		match.active = true;
+		// return $scope.firstMatch = match;
+	}
+	$scope.storeMatch = function(event, match){
+		if(event.isDetails){
+			return 
+		}
+		$scope.sources.second.forEach(function(match){
+			match.active = false;
+		});
+		
+		$scope.selectedMatch.second = match;
+		// $scope.secondMatch = match;
+		match.active = true;
+		
+		
+		var similarMatches = {
+			first: $scope.firstMatch,
+			second: $scope.secondMatch
+		};
+		similarMatches = JSON.stringify(similarMatches);
+		$scope.oursMatches.push(similarMatches);
+		$http.post('API/storage.json' , similarMatches);
+		$scope.deleteMatches();
+		// console.info(similarMatches);
+		// console.info($scope.sources.first.length);
 
 	}
-	$scope.changeLeague = function(event) {
-		console.log(event);
+	$scope.deleteMatches = function(){
+		if($scope.selectedMatch.first != null && $scope.selectedMatch.second != null){
+			var index1 = $scope.sources.first.indexOf($scope.selectedMatch.first);
+			var index2 = $scope.sources.second.indexOf($scope.selectedMatch.second);
+			$scope.sources.first.splice(index1, 1);
+			$scope.sources.second.splice(index2, 1);
+			$scope.selectedMatch.first = $scope.selectedMatch.second = null;	
+		}	
 	}
+	$scope.deleteOneMatch = function(match, source){
+		if(source == $scope.sources.first){
+			var index = source.indexOf(match);
+			$scope.sources.first.splice(index, 1);
+		}
+		if(source == $scope.sources.second){
+			var index = source.indexOf(match);
+			console.log(index);
+			$scope.sources.second.splice(index, 1);
+		}
+		// console.log(index);
+	}
+	
 	$scope.validateDate = function (date) {
 		var day = date.getDate();
 		var month = date.getMonth() + 1;
@@ -63,14 +129,13 @@ APP.controller('Panel', function($scope){
 		}
 		return day + '.' + month + '.' + year;
 	}
-	// $scope.validateDate($scope.match_day);
 
-	$scope.translationOnPanel = function(){
-		// var source = angular.element(querySelector('.left_list table tbody tr').length);
-		// console.log(source);
-		// var elem = angular.element(document.querySelector(".left_list thead tr td"));
-		// console.log(elem.html());
+	$scope.openMatch = function(event, match, source){
+		event.isDetails = true;
+		$scope[source] = match;
+		console.log($scope[source])
 	}
-	$scope.translationOnPanel();
-
+	
+	console.log($scope.betExp);
+	$scope.init();
 })
